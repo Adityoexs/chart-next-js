2c71e9bc97996fd415fc1f0e984eb263d802fed5
DESKTOP-UTITL9B\HP <adityo.eos@gmail.com>
2023-11-25 15:06:25 +0700

    chart commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,133 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { Line } from "react-chartjs-2";
 
-export default function Home() {
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale, // x axis
+  LinearScale, // y axis
+  PointElement,
+  Legend,
+  Tooltip,
+  Filler,
+} from "chart.js";
+
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Legend,
+  Tooltip,
+  Filler
+);
+
+const salesData = [
+  { month: "January", sales: 150 },
+  { month: "February", sales: 120 },
+  { month: "March", sales: 210 },
+  { month: "April", sales: 150 },
+  { month: "May", sales: 190 },
+  { month: "June", sales: 260 },
+  { month: "July", sales: 150 },
+  { month: "August", sales: 120 },
+  { month: "September", sales: 210 },
+  { month: "October", sales: 160 },
+  { month: "November", sales: 200 },
+  { month: "December", sales: 280 },
+];
+
+function LineChart() {
+  const data = {
+    labels: salesData.map((data) => data.month),
+    datasets: [
+      {
+        label: "Revenue",
+        data: salesData.map((data) => data.sales),
+        borderColor: "#32de84",
+        borderWidth: 3,
+        pointBorderColor: "#006400",
+        pointBorderWidth: 3,
+        tension: 0.5,
+        fill: true,
+        backgroundColor: (context) => {
+          const ctx = context.chart.ctx;
+          const gradient = ctx.createLinearGradient(0, 0, 0, 300);
+          gradient.addColorStop(0, "#66FF00");
+          gradient.addColorStop(1, "white");
+          return gradient;
+        },
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: true,
+    },
+    responsive: true,
+    scales: {
+      y: {
+        ticks: {
+          font: {
+            size: 17,
+            weight: "bold",
+          },
+        },
+        title: {
+          display: true,
+          text: "Sales",
+          padding: {
+            bottom: 10,
+          },
+          font: {
+            size: 30,
+            style: "italic",
+            family: "Arial",
+          },
+        },
+        min: 50,
+      },
+      x: {
+        ticks: {
+          font: {
+            size: 17,
+            weight: "bold",
+          },
+        },
+        title: {
+          display: true,
+          text: "Month",
+          padding: {
+            top: 10,
+          },
+          font: {
+            size: 30,
+            style: "italic",
+            family: "Arial",
+          },
+        },
+      },
+    },
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div>
+      <h1 className="font-bold text-3xl text-center mt-10">
+        Chart Flow using ChartJS
+      </h1>
+      <div
+        style={{
+          width: "1500px",
+          height: "550px",
+          padding: "20px",
+          cursor: "pointer",
+        }}
+      >
+        <Line data={data} options={options}></Line>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
+
+export default LineChart;
